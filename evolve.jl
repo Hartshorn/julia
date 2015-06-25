e408b2d9efc00c99f0b54c98330b0390b0b0f7e3
michael <mpalmer16@gmail.com>
2015-06-24 22:49:01 -0500

    changes

--- evolve.jl
+++ evolve.jl
@@ -1,36 +1,43 @@
 const WIDTH  = 10
 const HEIGHT = 10
 const LENGTH = WIDTH * HEIGHT
-const GENES  = Int[1,2,3,4,5,6,7,8]
+# const GENES  = Int[1,2,3,4,5,6,7,8]
 const PLANTENERGY = 80
 
 type Animal
-    x::Int
-    y::Int
     energy
     direction
     genes
 end
 
-animals = Array{Animal}(LENGTH)
-plants = zeros(Int, LENGTH)
-
-firstanimal = Animal(WIDTH / 2, HEIGHT / 2,
-                     500, 0, rand(GENES, 8))
-
-function animal_eat(animal::Animal, plants::Array)
-    if plants[get_loc(animal.x, animal.y)] != 0
-        animal.energy += PE
-    end
+type Pos
+	hasA::Bool
+	hasP::Bool
+	Animal::Animal
 end
 
-function add_animal_at_loc(animals::Array, animal::Animal)
-    animals[get_loc(animal.x, animal.y)] = animal
+# something strange here - everything updates the same...
+positions = fill(Pos(false, 
+					  false, 
+					  Animal(0,0,[0,0,0,0,0,0,0,0])), 
+				  LENGTH)
+
+firstanimal = Animal(500, 0, abs(rand(Int, 8)) % 8)
+
+function setpos(positions::Array, x::Int, y::Int,
+				hasp::Bool, hasa::Bool, 
+				animal::Animal...)
+	loc = get_loc(x,y)
+	if hasp
+		positions[loc].hasP = true
+	end
+	if hasa
+		positions[loc].hasA = true
+		positions[loc].Animal = animal[1]
+	end
 end
 
-get_loc(x::Int, y::Int) = round(Int, WIDTH * y + x)
+
+get_loc(x::Int, y::Int) = WIDTH * y + x
 
 gen_rand_int() = round(Int, rand() * 1000) % LENGTH
-
-add_random_plants() = plants[gen_rand_int()] = 1
-
